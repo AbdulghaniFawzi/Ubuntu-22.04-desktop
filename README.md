d2d2d9f33e6831a0a4e9fcee7996128277b4d12e
AbdulghaniFawzi <abdelgahnifawzi@gmail.com>
2023-06-16 00:42:59 +0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ubuntu-22.04-desktop
+Ubuntu-22.04-desktop
+
+``` bash
+curl  -L -o u22.sh https://raw.githubusercontent.com/abdulghanifawzi/Ubuntu-22.04-desktop/main/u22.sh && chmod +x u22.sh && ./u22.sh
+```
---
+++ u22.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+clear
+
+cat << "EOF"
+ ______ _ _     _
+|  ____| | |   | |                     /\
+| |__  | | |__ | | __ _ ___ _   _     /  \   _ __  _ __
+|  __| | | '_ \| |/ _` / __| | | |   / /\ \ | '_ \| '_ \
+| |____| | |_) | | (_| \__ \ |_| |  / ____ \| |_) | |_) |
+|______|_|_.__/|_|\__,_|___/\__, | /_/    \_\ .__/| .__/
+                             __/ |          | |   | |
+                            |___/           |_|   |_|
+EOF
+    sleep 6s
+
+	    sudo dd if=/dev/zero of=/swapfile bs=1M count=2048 status=progress
+	    sudo chmod 600 /swapfile
+	    sudo mkswap /swapfile
+	    sudo swapon /swapfile
+	    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+
+
+       sudo wget https://download.nomachine.com/download/8.4/Linux/nomachine_8.4.2_1_amd64.deb
+
+
+
+
+
+    sudo adduser abdulghanifawzi
+         #(example password : paste  se7ye8pc5hs0  )
+    sudo usermod -aG sudo,adm,lp,sys,lpadmin abdulghanifawzi
+
+# MAIN ROUTINE FOLLOWS #
+# ---------------------#
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install ubuntu-desktop -y
+sudo apt-get install stacer -y
+sudo apt-get install mmv -y
+sudo apt-get install firefox -y
+sudo apt-get install qdirstat -y
+sudo apt install -f ./nomachine_8.4.2_1_amd64.deb
+sudo usermod -aG abdulghanifawzi
+sudo reboot
